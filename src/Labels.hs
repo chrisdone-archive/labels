8ec370bd02715ce5035f7da3fe01bee41398eae3
Chris Done <chrisdone@gmail.com>
2016-08-08 13:12:28 +0100

    Updates

--- src/Labels.hs
+++ src/Labels.hs
@@ -1,12 +1,61 @@
 -- | Labels for fields in a tuple.
+--
+-- Enable these extensions:
+--
+-- In GHCi:
+--
+-- @
+-- :set -XOverloadedLabels -XTypeOperators -XDataKinds -XFlexibleContexts
+-- @
+--
+-- In a module:
+--
+-- @
+-- {-\# LANGUAGE OverloadedLabels, TypeOperators, DataKinds, FlexibleContexts \#-}
+-- @
+--
+-- Construct a record:
+--
+-- >>> (#foo := "hi", #bar := 123)
+-- (#foo := "hi",#bar := 123)
+--
+-- Get fields of a record:
+--
+-- >>> get #bar (#foo := "hi", #bar := 123)
+-- 123
+--
+-- Set fields of a record:
+--
+-- >>> set #bar 66 (#foo := "hi", #bar := 123)
+-- (#foo := "hi",#bar := 66)
+--
+-- Modify fields of a record:
+--
+-- >>> modify #mu (*0.1) (#bar := "hi", #mu := 123)
+-- (#bar := "hi",#mu := 12.3)
+--
+-- Add fields to a record:
+--
+-- >>> cons (#mu := [1,2,3]) (#foo := "hi", #bar := 123)
+-- (#mu := [1,2,3],#foo := "hi",#bar := 123)
+--
+-- Abstraction:
+--
+-- >>> let double field record = set field (get field record * 2) record
+-- >>> double #mu (#bar := "hi", #mu := 123)
+-- (#bar := "hi",#mu := 246)
 
 module Labels
   (-- Field access
    get
+  ,set
+  ,modify
+  ,cons
    -- Construction
-   ,(:=)(..)
-   ,Has
-   )
+  ,(:=)(..)
+  ,Has
+  ,Cons
+  )
   where
 
 import Labels.Internal
--- src/Labels/Internal.hs
+++ src/Labels/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE GADTs #-}
@@ -44,7 +45,54 @@ instance (Show t) => Show (l := t) where
 
 class Has (label :: Symbol) record value | label record -> value where
   -- | Get a field by doing: @get #salary employee@
-  get :: record -> value
+  get :: Proxy label -> record -> value
+  -- | Set a field by doing: @set #salary 54.00 employee@
+  set :: Proxy label -> value -> record -> record
+
+-- | Modify a field by doing: @modify #salary (* 1.1) employee@
+modify :: Has label record t => Proxy label -> (t -> t) -> record -> record
+modify f g r = set f (g (get f r)) r
+
+class Cons label value record where
+  type Consed label value record
+  -- | Cons a field onto a record by doing: @cons (#foo := 123) record@
+  cons :: (label := value) -> record -> Consed label value record
+
+instance Cons label value () where
+  type Consed label value () = (label := value)
+  cons field () = field
+
+instance Cons label value (label' := value') where
+  type Consed label value (label' := value') = (label := value,label' := value')
+  cons field field2 = (field,field2)
+
+$(let makeInstance size =
+        [d|instance Cons $(varT label_tyvar) $(varT value_tyvar) $tupTy where
+             type Consed $(varT label_tyvar) $(varT value_tyvar) $tupTy = $newTupTy
+             cons $(varP field_name) $tupPat = $tupEx|]
+        where label_tyvar = mkName "label"
+              value_tyvar = mkName "value"
+              field_name = mkName "field"
+              tupPat = tupP (map (\j -> varP (mkName ("v" ++ show j))) [1..size])
+              tupEx = tupE (varE field_name : map (\j -> varE (mkName ("v" ++ show j))) [1..size])
+              newTupTy =
+                  foldl
+                      appT
+                      (tupleT (size+1))
+                      ((infixT (varT label_tyvar) ''(:=) (varT value_tyvar)) :
+                       (map
+                            (\j ->
+                                  varT (mkName ("u" ++ show j)))
+                            [1 .. size]))
+              tupTy =
+                foldl
+                    appT
+                    (tupleT size)
+                    (map
+                         (\j ->
+                               varT (mkName ("u" ++ show j)))
+                         [1 .. size])
+  in fmap concat (mapM makeInstance [2 .. 24]))
 
 instance IsString (Q Exp) where
   fromString str = [|Proxy :: Proxy $(litT (return (StrTyLit str)))|]
@@ -52,11 +100,13 @@ instance IsString (Q Exp) where
 instance l ~ l' => IsLabel (l :: Symbol) (Proxy l') where
   fromLabel _ = Proxy
 
-instance Has l r a => IsLabel (l :: Symbol) (r -> a) where
-  fromLabel _ = get @l
+-- instance Has l r a => IsLabel (l :: Symbol) (r -> a) where
+--   fromLabel _ = get @l
 
 $(let makeInstance size slot =
-          [d|instance Has $(varT l_tyvar) $instHead $(varT a_tyvar) where get = $getImpl|]
+          [d|instance Has $(varT l_tyvar) $instHead $(varT a_tyvar)
+               where get _ = $getImpl
+                     set _ = $setImpl|]
         where
           l_tyvar = mkName "l"
           a_tyvar = mkName "a"
@@ -66,6 +116,33 @@ $(let makeInstance size slot =
                               [1 .. size])]
                   (varE a_var)
             where a_var = mkName "a"
+          setImpl =
+            lamE
+                [ varP v_var
+                ,tupP
+                     (map
+                          (\j ->
+                                if j == slot
+                                   then infixP
+                                            (varP (nth_proxy_var j))
+                                            '(:=)
+                                            wildP
+                                   else varP (nth_var j))
+                          [1 .. size])]
+                (tupE
+                      (map
+                           (\j ->
+                                 if j == slot
+                                    then appE
+                                             (appE (conE '(:=)) (varE (nth_proxy_var j)))
+                                             (if j == slot
+                                                  then varE v_var
+                                                  else varE (nth_var j))
+                                    else varE (nth_var j))
+                           [1 .. size]))
+            where nth_var i = mkName ("u_" ++ show i)
+                  nth_proxy_var i = mkName ("p_" ++ show i)
+                  v_var = mkName "v"
           instHead =
               foldl
                   appT
@@ -80,4 +157,4 @@ $(let makeInstance size slot =
          (concat . concat)
          (mapM (\size -> mapM (\slot -> makeInstance size slot)
                               [1 .. size])
-                [1 .. 24]))
+                [1 .. 16]))
